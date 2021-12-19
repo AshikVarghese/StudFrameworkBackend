d81632500bdb94454684c240449a26b4cfdccfda
sinking8 <ashwinblaze111@gmail.com>
2021-12-19 23:10:52 +0530

    updated

--- server.js
+++ server.js
@@ -1089,7 +1089,7 @@ app.post("/InternshipGraphCA", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () =>
   console.log(`Server running in port "${PORT}"`)
 );
