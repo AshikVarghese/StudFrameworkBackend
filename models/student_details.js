ad359b6c764d29b3b367e270c877c4f736310d24
Gautham S <gauthamsrinivasan2021@gmail.com>
2022-01-25 21:51:27 +0530

    department list type backend

--- models/student_details.js
+++ models/student_details.js
@@ -85,6 +85,17 @@ function fetch_students_details_official(callback) {
     }
   );
 }
+function fetch_students_details_official_department(callback) {
+  connection.query(
+    "SELECT * FROM `student`.`student_details` GROUP BY `dept` ",
+    (err, results, fields) => {
+      if (err) {
+        return callback(false);
+      }
+      return callback(results);
+    }
+  );
+}
 
 function insert_student_details(params, callback) {
   connection.query(
@@ -257,4 +268,5 @@ module.exports = {
   edit_student_details,
   delete_student_details,
   fetch_students_details_pd,
+  fetch_students_details_official_department,
 };
