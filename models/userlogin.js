eabb68a2c0f24036073076612894115af8299230
Sanjay03K <75990210+Sanjay03K@users.noreply.github.com>
2022-04-03 20:58:20 +0530

    admin

--- models/userlogin.js
+++ models/userlogin.js
@@ -11,7 +11,6 @@ function user_login(params, callback) {
       '";',
     (err, details, fields) => {
       if (err) {
-        console.log(err);
         return callback("user-fail");
       } else {
         if (details.length == 0) {
@@ -22,9 +21,7 @@ function user_login(params, callback) {
           .createHash("sha512")
           .update(params.password)
           .digest("hex");
-        console.log(password);
         if (password === details[0].password) {
-          console.log("Password matched with hashed password");
           var auth_token = crypto
             .createHash("sha512")
             .update(crypto.randomBytes(32).toString("hex"))
@@ -40,7 +37,6 @@ function user_login(params, callback) {
               '");',
             (err, results, fields) => {
               if (err) {
-                console.log(err);
                 return callback("server-down");
               } else {
                 return callback(details);
