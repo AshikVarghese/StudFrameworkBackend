a893abbdf6838baef1e9f4d06e467e04621e3314
LicetStudFramework <sdc@licet.ac.in>
2022-04-30 11:24:15 +0530

    dev_updated

--- server.js
+++ server.js
@@ -1329,7 +1329,7 @@ app.post("/admin_get_creds", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // const server = app.listen(PORT,() =>
 //   console.log(`Server running in port :"${PORT}"`)
