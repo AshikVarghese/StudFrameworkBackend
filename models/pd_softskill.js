58336c041ce9e67f5cba0961d09d6eedacb1b66b
LicetStudFramework <licetsdc@licet.ac.in>
2022-05-02 16:01:39 +0530

    soft_skills

---
+++ models/pd_softskill.js
@@ -0,0 +1,54 @@
+const connection = require("../config/dbconfig");
+
+function get_soft_skill(callback) {
+  connection.query(
+    "SELECT * from `pd_soft_skill` WHERE(roll_no=?)",
+    [params.RollNumber],
+    (err, results, fields) => {
+      if (err) {
+        console.log(err);
+      } else {
+        return callback(results);
+      }
+    }
+  );
+}
+
+function edit_soft_skill(params) {
+  connection.query("UPDATE `pd_soft_skill` SET trainer = ?, date = ?, remarks = ?, credits = ? WHERE (s_no = ?)",
+  [
+    params.assessment,
+    params.date,
+    params.remarks,
+    params.credits
+  ],
+  (err, results, fields) => {
+    if (err) {
+      console.log(err);
+      return callback(false);
+    } else {
+      return callback(results);
+    }
+   }
+  );
+}
+
+function delete_soft_skill(callback) {
+    connection.query(
+        "DELETE FROM `pd_soft_skill` WHERE (s_no = ?)",
+        [params.columnid],
+        (err, results, fields) => {
+          if (err) {
+            return callback("Delete Failed");
+          } else {
+            return callback("Delete Success");
+          }
+        }
+      );
+}
+
+module.exports = {
+    edit_soft_skill : edit_soft_skill,
+    delete_soft_skill : delete_soft_skill,
+    get_soft_skill : get_soft_skill
+}
