f07e71ce884ab66d381a5964a586c7283fdc78d1
Ashwin Balasubramanian <56084309+sinking8@users.noreply.github.com>
2022-05-06 10:56:22 +0530

    Update academic_details.js

--- models/academic_details.js
+++ models/academic_details.js
@@ -73,8 +73,7 @@ function fetch_academic_columns(callback) {
 
 function fetch_academic_details_official(callback) {
   connection.query(
-    "SELECT * from academics inner join student_details on academics.roll_no=student_details.roll_no and student_details.dept=?",
-    [params.department],
+    "SELECT * from academics inner join student_details on academics.roll_no=student_details.roll_no",
     (err, results, fields) => {
       if (err) {
         return callback(false);
@@ -98,8 +97,8 @@ function fetch_academic_details_hod(params, callback) {
 }
 function fetch_academic_details_classadvisor(params, callback) {
   connection.query(
-    "SELECT * from academics inner join student_details on academics.roll_no=student_details.roll_no where student_details.batch=?",
-    [params.batch],
+    "SELECT * from academics inner join student_details on academics.roll_no=student_details.roll_no where student_details.batch=? and student_details.dept=?",
+    [params.batch,params.department],
     (err, results, fields) => {
       if (err) {
         return callback(false);
